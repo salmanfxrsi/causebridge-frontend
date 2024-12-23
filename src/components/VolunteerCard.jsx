a85fc33e3f8ef83de10a97466682efd8e9a61f49
salmanfxrsi <salmanxprivate@gmail.com>
2024-12-23 11:52:13 +0600

    feat: Add VolunteerCard component to display volunteer opportunities with details and view link

--- src/components/VolunteerCard.jsx
+++ src/components/VolunteerCard.jsx
@@ -1,22 +1,23 @@
-const VolunteerCard = () => {
+import { Link } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
+const VolunteerCard = ({ post }) => {
+  const { thumbnail, postTitle, deadline, category, description } = post;
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
+        <img src={thumbnail} alt="Shoes" className="h-[200px] w-full" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <h2 className="card-title">{postTitle}</h2>
+        <p>{description}</p>
+        <div className="card-actions justify-start">
+          <div className="badge badge-outline">Deadline: {deadline}</div>
+          <div className="badge badge-outline">{category}</div>
         </div>
+        <Link className="bg-[#52C303] w-full text-sm font-medium text-white capitalize transition-colors duration-300 transform lg:w-auto hover:bg-gray-500 focus:outline-none focus:bg-gray-500 mt-4 text-center py-1 rounded-sm">
+          View Details
+        </Link>
       </div>
     </div>
   );
--- src/pages/AllVolunteerPost.jsx
+++ src/pages/AllVolunteerPost.jsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import VolunteerCard from "../components/VolunteerCard";
+import { useState } from "react";
+import axios from "axios";
 
 const AllVolunteerPost = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    fetchPosts()
+  },[])
+
+  // fetching all posts from db
+  const fetchPosts = async () => {
+    const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/volunteer-posts`)
+    setPosts(data)
+  }
+
   return (
-    <section className="my-16 container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      <VolunteerCard></VolunteerCard>
-      <VolunteerCard></VolunteerCard>
-      <VolunteerCard></VolunteerCard>
-      <VolunteerCard></VolunteerCard>
-      <VolunteerCard></VolunteerCard>
-      <VolunteerCard></VolunteerCard>
+    <section className="my-16">
+      <section className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {posts.map(post => <VolunteerCard key={post._id} post={post}></VolunteerCard>)}
+      </section>
     </section>
   );
 };
